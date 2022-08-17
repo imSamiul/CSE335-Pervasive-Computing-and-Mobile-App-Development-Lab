cabb5848d62d3eb0a68c580799d9ce0b8c9d81a3
SamiulKarim <samiulkarimprodhan@gmail.com>
2022-08-17 22:20:12 +0600

    finished calculator project

--- ProjectCourse/app/src/main/java/com/example/firstapplication/MainActivity.java
+++ ProjectCourse/app/src/main/java/com/example/firstapplication/MainActivity.java
@@ -3,18 +3,29 @@ package com.example.firstapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
-    EditText nameEditText;
+//    EditText nameEditText;
+    EditText firstValue;
+    EditText secondValue;
+    TextView showResult;
+    private static final DecimalFormat df = new DecimalFormat("0.00");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        nameEditText = findViewById(R.id.nameEditTextId);
+//        nameEditText = findViewById(R.id.nameEditTextId);
+        firstValue = findViewById(R.id.firstValue);
+        secondValue = findViewById(R.id.secondValue);
+        showResult = findViewById(R.id.showResult);
     }
 
 
@@ -22,9 +33,38 @@ public class MainActivity extends AppCompatActivity {
 //        Toast.makeText(this, "Button Clicked", Toast.LENGTH_SHORT).show();
 //    }
 
-    public void submitFunction(View view) {
-        String name = nameEditText.getText().toString();
-        Toast.makeText(this, "Welcome " + name, Toast.LENGTH_SHORT).show();
+//    public void submitFunction(View view) {
+//        String name = nameEditText.getText().toString();
+//        Toast.makeText(this, "Welcome " + name, Toast.LENGTH_SHORT).show();
+//
+//    }
 
+    public void onPlus(View view) {
+        double fValue = Double.parseDouble(firstValue.getText().toString());
+        double sValue =Double.parseDouble( secondValue.getText().toString());
+        double result =(fValue+sValue);
+        showResult.setText(df.format(result));
+
+    }
+
+    public void onMinus(View view) {
+        double fValue = Double.parseDouble(firstValue.getText().toString());
+        double sValue =Double.parseDouble( secondValue.getText().toString());
+        double result =(fValue-sValue);
+        showResult.setText(df.format(result));
+    }
+
+    public void onMultiply(View view) {
+        double fValue = Double.parseDouble(firstValue.getText().toString());
+        double sValue =Double.parseDouble( secondValue.getText().toString());
+        double result =(fValue*sValue);
+        showResult.setText(df.format(result));
+    }
+
+    public void onDivide(View view) {
+        double fValue = Double.parseDouble(firstValue.getText().toString());
+        double sValue =Double.parseDouble( secondValue.getText().toString());
+        double result =(fValue/sValue);
+        showResult.setText(df.format(result));
     }
 }
