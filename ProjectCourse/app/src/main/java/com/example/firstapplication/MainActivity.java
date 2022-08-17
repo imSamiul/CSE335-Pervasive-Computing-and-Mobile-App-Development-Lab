4539eda6aa1f83de9cef43007bc1f6824043645f
SamiulKarim <samiulkarimprodhan@gmail.com>
2022-08-17 20:07:24 +0600

    05+06 Toast and Edit Text XML+Java Done

--- ProjectCourse/app/src/main/java/com/example/firstapplication/MainActivity.java
+++ ProjectCourse/app/src/main/java/com/example/firstapplication/MainActivity.java
@@ -4,18 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    EditText nameEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        nameEditText = findViewById(R.id.nameEditTextId);
     }
 
 
-    public void clickFunction(View view) {
-        Toast.makeText(this, "Button Clicked", Toast.LENGTH_SHORT).show();
+//    public void clickFunction(View view) {
+//        Toast.makeText(this, "Button Clicked", Toast.LENGTH_SHORT).show();
+//    }
+
+    public void submitFunction(View view) {
+        String name = nameEditText.getText().toString();
+        Toast.makeText(this, "Welcome " + name, Toast.LENGTH_SHORT).show();
+
     }
 }
